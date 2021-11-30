c9d06438fa400d366d7153fc3f170a2c111b622f
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-11-30 18:41:33 +0700

    add initialize create default folder when didn't exist

--- src/main/cruddatabuku/Main.java
+++ src/main/cruddatabuku/Main.java
@@ -2,10 +2,20 @@ package main.cruddatabuku;
 
 import main.cruddatabuku.screen.gui.Home;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+        // Membuat folder default untuk menampung file data
+        File defaultFolder = new File("data");
+        if (!defaultFolder.exists()) {
+            boolean created = defaultFolder.mkdir();
+            if (created) System.out.print("");
+        }
+
         // Jika ingin meilhat tampilan GUI tuliskan :
         new Home();
+
         // jika ingin melihat tampilan console tuliskan :
         // new MainMenu();
     }
